28d52b60c0e25812fcd74612bdc72f6a74d3fc17
Jordan Mungujakisa <72340216+jordan-jakisa@users.noreply.github.com>
2024-05-27 21:59:45 +0300

    Optimizations

--- main.py
+++ main.py
@@ -1,118 +1,83 @@
-import re
 import os
+import re
 import requests
 from bs4 import BeautifulSoup
 from langchain_community.tools import DuckDuckGoSearchResults
 from langchain_community.utilities.duckduckgo_search import DuckDuckGoSearchAPIWrapper
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import OpenAI
-import requests
-from bs4 import BeautifulSoup
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 openai_api_key = os.environ['OPENAI_API_KEY']
-
-keyword = "INTP personality type"
+gemini_api_key = os.environ['GEMINI_API_KEY']
 
 llm = OpenAI(api_key=openai_api_key, max_tokens=2000)
-
+#llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=gemini_api_key)
 
 def parse_links(search_results: str):
     print("-----------------------------------")
-    print("Parsing links...")
+    print("Parsing links ...")
     return re.findall(r'link:\s*(https?://[^\],\s]+)', search_results)
 
 
-def get_h2_elements(url: str):
-    print("-----------------------------------")
-    print("Getting h2 elements...")
-    web_response = requests.get(url)
-    web_response.raise_for_status()
-
-    soup = BeautifulSoup(web_response.content, 'html.parser')
-
-    h2_elements = soup.findAll('h2')
-
-    h2_texts = [h2.get_text() for h2 in h2_elements]
-
-    return h2_texts
-
-
-def get_brainstorm_questions(keyword: str, h2_elements: str):
-    print("-----------------------------------")
-    print("Generating brainstorm questions...")
-
-    keyword = keyword
-    h2_elements = h2_elements
-
-    template = """
+def generate_blog(keyword, context):
+    try:
+        print("-----------------------------------")
+        print("Generating blog post ...")
+        template = """
+        Given the following information, generate a blog post
         
-        {question}
-    
-        Generate 10 questions to write an SEO optimized blog post on this !keyword 
-
-        Similar top performing blog posts have the following h2 elements: !h2_elements
+        Write a full blog post that will rank for the following keywords: {keyword}
         
-        Return 10 answers in a numbered format and keep them brief and relevant to the topics  
-              
-    """
-
-    template = template.replace("!keyword", keyword).replace("!h2_elements", h2_elements)
-
-    prompt = PromptTemplate.from_template(template)
-
-    chain = prompt | llm
-
-    question_response = chain.invoke("")
-
-    return question_response
-
-def generate_blog(keyword=keyword):
-    print("-----------------------------------")
-    print("Generating blog outline...")
-    template = """
-    {question}
-    
-    Write a full blog post that will rank for the following keywords: !keyword
-    
+        Instructions:
         
-    Instructions:
-    
-    The blog should be in properly and beautifully formatted using markdown.
+        The blog should be properly and beautifully formatted using markdown.
+            
+        The blog title should be SEO optimized.
         
-    The blog title should be SEO optimized.
-    
-    Each sub-section should have least 3 paragraphs.
-    
-    Each section should have at least three subsection.
-    
-    Sub-section headings should be clearly marked.
-    
-    Clearly indicate the title, headings and sub-headings using markdown.
+        Each sub-section should have at least 3 paragraphs.
+        
+        Each section should have at least three subsections.
+        
+        Sub-section headings should be clearly marked.
+        
+        Clearly indicate the title, headings, and sub-headings using markdown.
 
-    Each section should cover the specific aspects as outlined.
+        Each section should cover the specific aspects as outlined.
 
-    For each section, generate detailed content that aligns with the provided subtopics. Ensure that the content is informative and covers the key points.
+        For each section, generate detailed content that aligns with the provided subtopics. Ensure that the content is informative and covers the key points.
 
-    Ensure that the content flows logically from one section to another, maintaining coherence and readability.
+        Ensure that the content flows logically from one section to another, maintaining coherence and readability.
 
-    Where applicable, include examples, case studies, or insights that can provide a deeper understanding of the topic.
+        Where applicable, include examples, case studies, or insights that can provide a deeper understanding of the topic.
 
-    Always include discussions on ethical considerations, especially in sections dealing with data privacy, bias, and responsible use.
+        Always include discussions on ethical considerations, especially in sections dealing with data privacy, bias, and responsible use.
 
-    In the final section, provide a forward-looking perspective on the topic and a conclusion.
-    
-    Please, ensure proper and standard markdown formatting always.
-    """
+        In the final section, provide a forward-looking perspective on the topic and a conclusion.
+        
+        Please ensure proper and standard markdown formatting always.
+        
+        Context:
 
-    template = template.replace("!keyword", keyword)
+        The following information is summaries from blog posts with similar titles, use it to inform your writing style and content.
+        
+        {context}
+        
+        """
 
-    prompt = PromptTemplate.from_template(template)
-    chain = prompt | llm
+        prompt = PromptTemplate(template=template, input_variables=["keyword", "context"])
+        chain = prompt | llm
 
-    outline_response = chain.invoke(f"Given the following information, genenrate a blog post")
+        outline_response = chain.invoke({"keyword": keyword, "context": context})
 
-    return outline_response
+        return outline_response
 
+    except Exception as e:
+        print(f"An error occurred while generating the blog post: {e}")
+        return None
 
 def save_file(content: str, filename: str):
     print("-----------------------------------")
@@ -121,40 +86,46 @@ def save_file(content: str, filename: str):
         f.write(content)
     print(f" 🥳 File saved as {filename}")
 
+def get_links(keyword):
+    try:
+        print("-----------------------------------")
+        print("Getting links ...")
 
-def get_headings() -> str:
+        wrapper = DuckDuckGoSearchAPIWrapper(max_results=2)
+        search = DuckDuckGoSearchResults(api_wrapper=wrapper)
+        results = search.run(tool_input=keyword)
+
+        links = []
+
+        for link in parse_links(results):
+            links.append(link)
+        
+        return links
+    
+    except Exception as e:
+        print(f"An error occurred while getting links: {e}")
+        return []
+
+def get_text_content(link):
     print("-----------------------------------")
-    print("Getting headings...")
-
-    wrapper = DuckDuckGoSearchAPIWrapper(max_results=3)
-    search = DuckDuckGoSearchResults(api_wrapper=wrapper)
-    results = search.run(tool_input=keyword)
-
-    headings = []
-
-    for link in parse_links(results):
-        get_page_text(link)
-        headings.append(get_h2_elements(link))
-
-    flat_headings = [item for sublist in headings for item in sublist]
-
-    return ', '.join(flat_headings)
-
-
-def get_page_text(link):
-    print("-----------------------------------")
-    print(f"Getting page text for ==> {link}...")
+    print(f"Getting page content from {link} ...")
+    
     try:
         response = requests.get(link)
         response.raise_for_status()
 
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        text_components = soup.find_all(text=True)
+        text_components = soup.find_all(['p', 'h1', 'h2', 'h3'])
+        
+        text = "\n".join([t.get_text() for t in text_components])
+        
+        print(text)
+        
+        summarize_page(text)
 
-        page_text = ' '.join(text_components)
-
-        return page_text
+        return text
+    
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while fetching the page: {e}")
         return ""
@@ -162,10 +133,48 @@ def get_page_text(link):
         print(f"An error occurred: {e}")
         return ""
 
+def summarize_page(text):
+    try:
+        print("-----------------------------------")
+        print(f"Summarizing page content ...")
+
+        prompt = PromptTemplate(
+            input_variables=["text"],
+            template="""
+                You are a proficient summarizer tasked with condensing blog posts into concise and neat three-paragraph summaries. Your summaries should preserve the key ideas, structure, and tone of the original post. Here’s what you need to do:
+
+                Read the provided blog post carefully.
+                Identify and extract the main ideas and supporting details.
+                Condense the information into three coherent paragraphs that retain the essence and flow of the original content.
+                Please ensure that the summary is clear, concise, and well-organized, capturing the original post's key points and maintaining its tone.
+
+                Summarize the following: 
+
+                {text}
+            """
+        )
+
+        chain = prompt | llm
+        response = chain.invoke(text)
+        print(f"Response: {response}")
+        return response
+
+    except Exception as e:
+        print(f"An error occurred while summarizing the page content: {e}")
+        return ""
+
+def main():
+    keyword = "Mobile development"
+    context = ""
     
+    links = get_links(keyword)
+    
+    for link in links:
+        text = get_text_content(link)
+        context += "\n" + text
+        
+    blog = generate_blog(keyword, context)
+    save_file(blog, keyword + ".md")
+    print("Success! 🎉")
 
-# headings = get_headings()
-# questions = get_brainstorm_questions(keyword, headings)
-blog_post = generate_blog()
-
-save_file(blog_post, f"{keyword}.md")
+main()
